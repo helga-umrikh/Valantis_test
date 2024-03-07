31fd69835418c2fca2c02fb45de0f181453af7c8
helga-umrikh <zetaumr@gmail.com>
2024-03-07 20:25:57 +0300

    feat: add routing and styles for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import './App.scss';
+import React from 'react'
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import ItemPage from './pages/ItemPage/ItemPage'
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path='/' element={<HomePage />}></Route>
+                <Route path='/item/:id' element={<ItemPage />}></Route>
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
